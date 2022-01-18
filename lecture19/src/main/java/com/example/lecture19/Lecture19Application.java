f2065ad2f351630b39d5cac38c20209e749f6892
Yavor Petkov <yavorpetkov92@gmail.com>
2022-01-18 19:57:51 +0200

    Lecture 19 initial commit

---
+++ lecture19/src/main/java/com/example/lecture19/Lecture19Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture19Application.class, args);
+	}
+
+}
---
+++ lecture19/src/test/java/com/example/lecture19/Lecture19ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture19;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture19ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
